a5d175b4e5355252499923110d379b0325fcde3e
criticalx7 <criticalx7@gmail.com>
2018-04-27 10:36:35 +0700

    Update login route, logic and ui.

---
+++ resources/views/layouts/empty.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title', 'LogMovie - Your Awesome Movie lists')</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+    <main class="">
+        @yield('content')
+    </main>
+</div>
+</body>
+</html>
