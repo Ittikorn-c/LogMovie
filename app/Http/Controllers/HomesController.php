dd5a71f79edad46aebd3d9ca7e2a98ac1b4377c7
ทัตพงศ์ วงศ์สมิทธิพันธุ์ <tatpong.w@ku.th>
2018-04-29 22:58:49 +0700

    controller

---
+++ app/Http/Controllers/HomesController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Home;
+use App\Movie;
+use App\News;
+use Illuminate\Http\Request;
+
+class HomesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $movies = Movie::orderBy('created_at', 'desc')->take(5)->get();
+        $news = News::orderBy('created_at', 'desc')->take(5)->get();
+        return view('home.index', ['movies' => $movies, 'news' => $news]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Home  $home
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Home $home)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Home  $home
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Home $home)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Home  $home
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Home $home)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Home  $home
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Home $home)
+    {
+        //
+    }
+}
