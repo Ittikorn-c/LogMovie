7ec198908face923d762e3b79e1c63f948216ab1
ทัตพงศ์ วงศ์สมิทธิพันธุ์ <tatpong.w@ku.th>
2018-04-30 14:42:23 +0700

    home

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -20,7 +20,7 @@
     font-weight: bold;
   }
   h3 {
-    border-bottom:  3px solid black;
+    /*border-bottom:  3px solid black;*/
   }
   .card {
       box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
@@ -35,10 +35,21 @@
 @endsection
 @section('content')
 <br>
-  <div class="body_main container">
-    <div class="text-content">
-      <h3>Lastest movies</h3>
-      <div class="row" style="margin-left: 5em">
+  <div class="body_main container" style="">
+    <div class="text-content" style="background-color:rgba(192,192,192,0.3);">
+      <h3>
+        <div class="row" style="background: orange;padding-left: 50px">
+          <div class="col-sm-6">
+              <mark style="background: white;padding: 5px">
+                Movies
+              </mark>
+          </div> 
+          <div class="col-sm-6" style="text-align: right;">
+            <a class="" href="movies/" style="font-size: 80%;display: inline;color: white">See all..</a>
+          </div> 
+        </div>
+      </h3>
+      <div class="row" style="margin-left: 5em;">
         @foreach ($movies as $movie)
           <div class="card" style="width:250px; margin-left: 3em; margin-top: 3em">
             <img class="card-img-top" src="{{ $movie->cover_image }}" alt="Card image" style="width:100%; height: 300px">
@@ -50,12 +61,24 @@
             </div>
           </div>
         @endforeach
+        <br>
       </div>
       <br>  
     </div>
-
-    <div class="text-content">
-      <h3>News <a href="news/" style="font-size: 70%">See all...</a></h3>
+    <br>
+    <div class="text-content" style="background-color:rgba(192,192,192,0.3);">
+      <h3>
+        <div class="row" style="background: orange;padding-left: 50px">
+          <div class="col-sm-6">
+              <mark style="background: white;padding: 5px">
+                News
+              </mark>
+          </div> 
+          <div class="col-sm-6" style="text-align: right;">
+            <a class="" href="news/" style="font-size: 80%;display: inline;color: white">See all..</a>
+          </div> 
+        </div>
+      </h3>
 
       @foreach ($news as $news)
         <div class="container" style="text-align: center;margin-top: 3em; border-bottom: 1px solid black">
